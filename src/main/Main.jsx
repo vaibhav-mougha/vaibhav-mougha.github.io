27b412d5bbfa741bab531b72fe69175800018396
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-04-27 09:40:54 +0530

    added Navbar IDs

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Content from '../Content/Content';
-import Navbar from '../SideBar/Navbar';
+import Navbar from '../Navbar/Navbar';
 import MainCSS from './Main.module.css';
 
 export default function Main() {
